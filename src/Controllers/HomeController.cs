51384f288893c33e1c7450179cb061e65c90a723
Anduin2017 <anduin@aiursoft.com>
2019-01-11 01:07:46 +0800

    Use the new layout.

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -5,14 +5,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Aiursoft.Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Aiursoft.Pylon.Services;
+using Aiursoft.Pylon;
+using Aiursoft.Pylon.Models;
 
 namespace Aiursoft.Blog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<BlogUser> _signInManager;
+        private readonly ILogger _logger;
+        private readonly ServiceLocation _serviceLocation;
+
+        public HomeController(
+            SignInManager<BlogUser> signInManager,
+            ILoggerFactory loggerFactory,
+            ServiceLocation serviceLocation)
+        {
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<HomeController>();
+            _serviceLocation = serviceLocation;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation(4, "User logged out.");
+            return this.SignoutRootServer(_serviceLocation.API, new AiurUrl(string.Empty, "Home", nameof(HomeController.Index), new { }));
         }
     }
 }
