ee54cc3b64ddc7bc5a460d352b71fa78cb9e5fe1
Anduin2017 <anduin@aiursoft.com>
2019-01-18 18:17:14 +0800

    Use view component to display profile card.

---
+++ src/Views/Shared/Components/ProfileCard/ProfileCard.cs
@@ -0,0 +1,36 @@
+﻿using Aiursoft.Blog.Data;
+using Aiursoft.Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Blog.Views.Shared.Components.ProfileCard
+{
+
+    public class ProfileCard : ViewComponent
+    {
+        private readonly BlogDbContext _dbContext;
+        private readonly UserManager<BlogUser> _userManager;
+        public ProfileCard(
+            BlogDbContext dbContext,
+            UserManager<BlogUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(UserClaimsPrincipal);
+            var model = new ProfileCardViewModel
+            {
+                NickName = user.NickName,
+                Bio = user.Bio
+            };
+            return View(model);
+        }
+    }
+}
---
+++ src/Views/Shared/Components/ProfileCard/ProfileCardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Blog.Views.Shared.Components.ProfileCard
+{
+    public class ProfileCardViewModel
+    {
+        public string NickName { get; set; }
+        public string Bio { get; set; }
+    }
+}
