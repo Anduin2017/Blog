1e50d17cc7023b243f562ff2ac069d7b83c618df
Anduin2017 <anduin@aiursoft.com>
2019-01-18 18:50:24 +0800

    Create me controller.

---
+++ src/Controllers/MeController.cs
@@ -0,0 +1,18 @@
+﻿using Aiursoft.Blog.Models.MeViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Blog.Controllers
+{
+    public class MeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexViewModel();
+            return View(model);
+        }
+    }
+}
---
+++ src/Models/MeViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Blog.Models.MeViewModels
+{
+    public class IndexViewModel : MeViewModel
+    {
+        public IndexViewModel()
+        {
+            Title = "Me";
+            Description = string.Empty;
+        }
+    }
+}
