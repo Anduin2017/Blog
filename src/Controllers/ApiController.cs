a770c96862ae68c1292534ee114e01386b35d2a1
Anduin2017 <anduin@aiursoft.com>
2019-01-24 11:36:34 +0800

    Create manifest json generator.

--- src/Controllers/ApiController.cs
+++ src/Controllers/ApiController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Aiursoft.Pylon.Attributes;
+using Microsoft.AspNetCore.Identity;
+using Aiursoft.Blog.Models;
 
 namespace Aiursoft.Blog.Controllers
 {
@@ -11,6 +13,36 @@ namespace Aiursoft.Blog.Controllers
     [APIModelStateChecker]
     public class ApiController : Controller
     {
-
+        private readonly UserManager<BlogUser> _userManager;
+        public ApiController(UserManager<BlogUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [Route("/manifest.json")]
+        public async Task<IActionResult> Manifest()
+        {
+            var owner = (await _userManager.GetUsersInRoleAsync(Consts.OwnerRoleName))
+                .First();
+            var model = new ManifestModel
+            {
+                ShortName = owner.NickName,
+                Name = owner.NickName + "'s Blog",
+                StartUrl = "/",
+                Icons = new List<ManifestIcon>()
+                {
+                    new ManifestIcon
+                    {
+                        Src = "",
+                        Sizes = "48x48",
+                        Type = "image/png"
+                    }
+                },
+                BackgroundColor = "#2391D3",
+                ThemeColor = "#2391D3",
+                Display = "standalone",
+                Orientation = "portrait"
+            };
+            return Json(model);
+        }
     }
 }
