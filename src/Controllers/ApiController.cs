fc5ccedc4859c6f2d9392ddc6a5ca1d03d427047
Anduin2017 <anduin@aiursoft.com>
2019-01-24 11:27:24 +0800

    Create API Controller.

---
+++ src/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aiursoft.Pylon.Attributes;
+
+namespace Aiursoft.Blog.Controllers
+{
+    [APIExpHandler]
+    [APIModelStateChecker]
+    public class ApiController : Controller
+    {
+
+    }
+}
