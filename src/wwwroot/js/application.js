2276adccd6aeb27dd4d6945869f972110fcc1f2c
Anduin2017 <anduin@aiursoft.com>
2019-01-24 19:14:33 +0800

    Fix some lint issues.

--- src/wwwroot/js/application.js
+++ src/wwwroot/js/application.js
@@ -23,16 +23,18 @@ $(function () {
 
 $(function () {
     function getRight() {
-        if (!$('[data-toggle="popover"]').length) return 0
-        return ($(window).width() - ($('[data-toggle="popover"]').offset().left + $('[data-toggle="popover"]').outerWidth()))
+        if (!$('[data-toggle="popover"]').length)
+            return 0;
+        var right = $(window).width() - ($('[data-toggle="popover"]').offset().left + $('[data-toggle="popover"]').outerWidth());
+        return right;
     }
 
     $(window).on('resize', function () {
-        var instance = $('[data-toggle="popover"]').data('bs.popover')
+        var instance = $('[data-toggle="popover"]').data('bs.popover');
         if (instance) {
-            instance.config.viewport.padding = getRight()
+            instance.config.viewport.padding = getRight();
         }
-    })
+    });
 
     $('[data-toggle="popover"]').popover({
         template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-body popover-content px-0"></div></div>',
@@ -45,40 +47,40 @@ $(function () {
             padding: getRight()
         },
         content: function () {
-            var $nav = $('#js-popoverContent').clone()
-            return '<ul class="nav nav-pills nav-stacked flex-column" style="width: 120px">' + $nav.html() + '</ul>'
+            var $nav = $('#js-popoverContent').clone();
+            return '<ul class="nav nav-pills nav-stacked flex-column" style="width: 120px">' + $nav.html() + '</ul>';
         }
-    })
+    });
 
     $('[data-toggle="popover"]').on('click', function (e) {
-        e.stopPropagation()
+        e.stopPropagation();
 
         if ($($('[data-toggle="popover"]').data('bs.popover').getTipElement()).hasClass('in')) {
-            $('[data-toggle="popover"]').popover('hide')
-            $(document).off('click.app.popover')
+            $('[data-toggle="popover"]').popover('hide');
+            $(document).off('click.app.popover');
 
         } else {
-            $('[data-toggle="popover"]').popover('show')
+            $('[data-toggle="popover"]').popover('show');
 
             setTimeout(function () {
                 $(document).one('click.app.popover', function () {
-                    $('[data-toggle="popover"]').popover('hide')
-                })
-            }, 1)
+                    $('[data-toggle="popover"]').popover('hide');
+                });
+            }, 1);
         }
-    })
+    });
 
-})
+});
 
 $(document).on('click', '.js-gotoMsgs', function () {
-    $input.remove()
-    $('.js-conversation').addClass('d-none')
-    $('.js-msgGroup, .js-newMsg').removeClass('d-none')
-    $('.modal-title').html('Messages')
-})
+    $input.remove();
+    $('.js-conversation').addClass('d-none');
+    $('.js-msgGroup, .js-newMsg').removeClass('d-none');
+    $('.modal-title').html('Messages');
+});
 
 $(document).on('click', '[data-action=growl]', function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
     $('#app-growl').append(
         '<div class="alert alert-dark alert-dismissible fade show" role="alert">' +
@@ -87,114 +89,114 @@ $(document).on('click', '[data-action=growl]', function (e) {
         '</button>' +
         'Click the x on the upper right to dismiss this little thing. Or click growl again to show more growls' +
         '</div>'
-    )
-})
+    );
+});
 
 $(document).on('focus', '[data-action="grow"]', function () {
     if ($(window).width() > 1000) {
         $(this).animate({
             width: 300
-        })
+        });
     }
-})
+});
 
 $(document).on('blur', '[data-action="grow"]', function () {
     if ($(window).width() > 1000) {
         var $this = $(this).animate({
             width: 180
-        })
+        });
     }
-})
+});
 
 // back to top button - docs
 $(function () {
-    if ($('.docs-top').length) {
-        _backToTopButton()
-        $(window).on('scroll', _backToTopButton)
-        function _backToTopButton() {
-            if ($(window).scrollTop() > $(window).height()) {
-                $('.docs-top').fadeIn()
-            } else {
-                $('.docs-top').fadeOut()
-            }
+    var _backToTopButton = function () {
+        if ($(window).scrollTop() > $(window).height()) {
+            $('.docs-top').fadeIn();
+        } else {
+            $('.docs-top').fadeOut();
         }
+    };
+
+    if ($('.docs-top').length) {
+        _backToTopButton();
+        $(window).on('scroll', _backToTopButton);
     }
-})
+});
 
 $(function () {
     // doc nav js
-    var $toc = $('#markdown-toc')
-    $('#markdown-toc li').addClass('nav-item')
-    $('#markdown-toc li > a').addClass('nav-link')
-    $('#markdown-toc li > ul').addClass('nav')
-    var $window = $(window)
+    var $toc = $('#markdown-toc');
+    $('#markdown-toc li').addClass('nav-item');
+    $('#markdown-toc li > a').addClass('nav-link');
+    $('#markdown-toc li > ul').addClass('nav');
+    var $window = $(window);
 
     if ($toc[0]) {
 
-        maybeActivateDocNavigation()
-        $window.on('resize', maybeActivateDocNavigation)
+        maybeActivateDocNavigation();
+        $window.on('resize', maybeActivateDocNavigation);
 
-        function maybeActivateDocNavigation() {
+        var maybeActivateDocNavigation = function () {
             if ($window.width() > 768) {
-                activateDocNavigation()
+                activateDocNavigation();
             } else {
-                deactivateDocNavigation()
+                deactivateDocNavigation();
             }
-        }
+        };
 
-        function deactivateDocNavigation() {
-            $window.off('resize.theme.nav')
-            $window.off('scroll.theme.nav')
+        deactivateDocNavigation = function () {
+            $window.off('resize.theme.nav');
+            $window.off('scroll.theme.nav');
             $toc.css({
                 position: '',
                 left: '',
                 top: ''
-            })
-        }
+            });
+        };
 
-        function activateDocNavigation() {
-
-            var cache = {}
+        var activateDocNavigation = function () {
+            var cache = {};
 
             function updateCache() {
-                cache.containerTop = $('.docs-content').offset().top - 40
-                cache.containerRight = $('.docs-content').offset().left + $('.docs-content').width() + 45
-                measure()
+                cache.containerTop = $('.docs-content').offset().top - 40;
+                cache.containerRight = $('.docs-content').offset().left + $('.docs-content').width() + 45;
+                measure();
             }
 
             function measure() {
-                var scrollTop = $window.scrollTop()
-                var distance = Math.max(scrollTop - cache.containerTop, 0)
+                var scrollTop = $window.scrollTop();
+                var distance = Math.max(scrollTop - cache.containerTop, 0);
 
                 if (!distance) {
-                    $($toc.find('li a')[1]).addClass('active')
+                    $($toc.find('li a')[1]).addClass('active');
                     return $toc.css({
                         position: '',
                         left: '',
                         top: ''
-                    })
+                    });
                 }
 
                 $toc.css({
                     position: 'fixed',
                     left: cache.containerRight,
                     top: 40
-                })
+                });
             }
 
-            updateCache()
+            updateCache();
 
             $(window)
                 .on('resize.theme.nav', updateCache)
-                .on('scroll.theme.nav', measure)
+                .on('scroll.theme.nav', measure);
 
             $('body').scrollspy({
                 target: '#markdown-toc'
-            })
+            });
 
             setTimeout(function () {
-                $('body').scrollspy('refresh')
-            }, 1000)
-        }
+                $('body').scrollspy('refresh');
+            }, 1000);
+        };
     }
-})
+});
