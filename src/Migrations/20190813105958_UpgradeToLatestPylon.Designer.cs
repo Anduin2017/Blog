cc312614aa3000975863fb340430944f7fb205a0
Anduin2017 <anduin@aiursoft.com>
2019-08-13 19:01:40 +0800

    Upgrade

--- src/Migrations/20190813105958_UpgradeToLatestPylon.Designer.cs
+++ src/Migrations/20190813105958_UpgradeToLatestPylon.Designer.cs
@@ -4,14 +4,16 @@ using Aiursoft.Blog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aiursoft.Blog.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190813105958_UpgradeToLatestPylon")]
+    partial class UpgradeToLatestPylon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrations/20190813105958_UpgradeToLatestPylon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aiursoft.Blog.Migrations
+{
+    public partial class UpgradeToLatestPylon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconFilePath",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconFilePath",
+                table: "AspNetUsers");
+        }
+    }
+}
